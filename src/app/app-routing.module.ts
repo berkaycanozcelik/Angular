eb98f79b55d09f3735c3c4e6e7025ab9f4afd1de
DESKTOP-6588BFP\Berkay Can Özçelik <brkycn10@gmail.com>
2022-03-16 17:58:13 +0300

    passing static data to a route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AutGuard } from "./auth-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
@@ -25,10 +26,15 @@ const appRoutes: Routes = [
     component: ServersComponent,
     children: [
       { path: ":id", component: ServerComponent },
-      { path: ":id/edit", component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
+      {
+        path: ":id/edit",
+        component: EditServerComponent,
+        canDeactivate: [CanDeactivateGuard],
+      },
     ],
   },
-  { path: "not-found", component: PageNotFoundComponent },
+ // { path: "not-found", component: PageNotFoundComponent },
+  { path: "not-found", component: ErrorPageComponent, data: {message: 'Page not found! :('}},
   { path: "**", redirectTo: "/not-found" }, //must be the last one in all routes defined
 ];
 
